e64394e55bb3b2a984519b792b96623a34244e3b
Federico Alvarez <fedvhast@gmail.com>
2022-03-10 01:01:23 +0100

    New algorithm (makeNegative)

---
+++ Algorithms/returnNegative.js
@@ -0,0 +1,18 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+/* Examples
+
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5
+makeNegative(0);    // return 0
+makeNegative(0.12); // return -0.12
+
+*/
+
+const makeNegative = (num) => {
+  if (num > 0) {
+    // Math.abs() function returns the absolute value of a number
+    return -Math.abs(num);
+  }
+  return num;
+} 
